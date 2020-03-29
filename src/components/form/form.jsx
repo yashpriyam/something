b2754bb0ceea563e7dee265adf981eac735189e4
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-03-29 23:15:42 +0530

    some clean up

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -94,80 +94,19 @@ class Form extends React.Component {
                                 {/* <Collapsible trigger="User Name" triggerTagName="fieldset">
                                     <div className='userinfo'>
                                         <label> name </label>
-                                        <input name='date' type='text' value='Girish i R' readOnly/>               
-                                        <label> id </label>
-                                        <input name='date' type='text' value='123456789' readOnly/>               
-                                        <label> email </label>
-                                        <input name='date' type='text' value='girishce4455@gmail.com' readOnly/>
-                                        <label> link </label>
-                                        <input name='date' type='text' value='new' readOnly/>
+                                        <input name='date' type='text' value='Girish i R' readOnly/> 
                                         <label> resume </label>
                                         <input name='date' type='text' value='https://www.jotform.me/uploads/sankarkrishna/72490348973467/3808526968862907492/Sowjanya Atyam.docx' readOnly/>
                                         <label> Email </label>
                                         <input name='date' type='text' value='123456789' readOnly/>
-                                        <label> First_Name </label>
-                                        <input name='date' type='text' value='First_Name' readOnly/>
-                                        <label> Last_Name </label>
-                                        <input name='date' type='text' value='Last_Name' readOnly/>
-                                        <label> Phone_Number </label>
-                                        <input name='date' type='text' value='Phone_Number' readOnly/>
                                         <label> How_did_you_come_by_this_Job_Opportunity </label>
                                         <input name='date' type='text' value='How_did_you_come_by_this_Job_Opportunity' readOnly/>
-                                        <label> Where_did_you_see_our_Ad </label>
-                                        <input name='date' type='text' value='Where_did_you_see_our_Ad' readOnly/>
-                                        <label> Which_of_our_recruitment_partners_redirected_you_here </label>
-                                        <input name='date' type='text' value='Which_of_our_recruitment_partners_redirected_you_here' readOnly/>
-                                        <label> Which_Social_Medium_did_you_notice_us_on </label>
-                                        <input name='date' type='text' value='Which_Social_Medium_did_you_notice_us_on' readOnly/>
-                                        <label> Which_Job_Portal_did_you_find_our_listing_on </label>
-                                        <input name='date' type='text' value='Which_Job_Portal_did_you_find_our_listing_on' readOnly/>
-                                        <label> Referrer_Name </label>
-                                        <input name='date' type='text' value='Referrer_Name' readOnly/>
-                                        <label> Referrer_Employee_ID </label>
-                                        <input name='date' type='text' value='Referrer_Employee_ID' readOnly/>
-                                        <label> Upload_your_CV_Resume </label>
-                                        <input name='date' type='text' value='Upload_your_CV_Resume' readOnly/>
-                                        <label> Have_you_ever_downloaded_and_used_BYJUS_The_Learning_App </label>
-                                        <input name='date' type='text' value='Have_you_ever_downloaded_and_used_BYJUS_The_Learning_App' readOnly/>
-                                        <label> Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas </label>
-                                        <input name='date' type='text' value='Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas' readOnly/>
-                                        <label> Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas </label>
-                                        <input name='date' type='text' value='Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas' readOnly/>
-                                        <label> Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas </label>
-                                        <input name='date' type='text' value='Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas' readOnly/>
-                                        <label> Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas </label>
-                                        <input name='date' type='text' value='Grades_1_to_12_What_is_your_proficiency_level_in_the_following_areas' readOnly/>
-                                        <label> What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below </label>
-                                        <input name='date' type='text' value='What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below' readOnly/>
-                                        <label> What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below </label>
-                                        <input name='date' type='text' value='What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below' readOnly/>
-                                        <label> What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below </label>
-                                        <input name='date' type='text' value='What_is_your_interest_level_in_contributing_to_the_grade_groups_listed_below' readOnly/>
-                                        <label> Why_do_you_want_to_join_BYJUS </label>
-                                        <input name='date' type='text' value='Why_do_you_want_to_join_BYJUS' readOnly/>
-                                        <label> Upload_your_task </label>
-                                        <input name='date' type='text' value='Upload_your_task' readOnly/>
-                                        <label> IP </label>
-                                        <input name='date' type='text' value='IP' readOnly/>
                                         <label> Edit_Link </label>
                                         <input name='date' type='text' value='Edit_Link' readOnly/>
                                     </div>
                                 </Collapsible>                          */}
                             </fieldset>
                     </div>
-                    
-                    {/* <div class="form-style-3">
-                        <div className="grid-item"><div className="form-style-10"><div className="section"><span>1</span></div></div></div>
-                            <InterviewCards/>
-                        </div>
-                    <div class="form-style-3">
-                        <div className="grid-item"><div className="form-style-10"><div className="section"><span>2</span></div></div></div>
-                            <InterviewCards/>  
-                        </div>
-                    <div class="form-style-3">
-                        <div className="grid-item"><div className="form-style-10"><div className="section"><span>3</span></div></div></div>
-                            <InterviewCards/>
-                        </div> */}
 
                         {
                             this.state.associationListCount.map((item)=>{
