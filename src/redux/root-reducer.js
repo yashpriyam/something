86e29485a955f6a3c8da7283a4f8cc400bc65bbe
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-03-30 10:56:05 +0530

    removed redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,19 +1,25 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';  //use 'storage' for using local storage
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';  //use 'storage' for using local storage
 
 import userReducer from './user/user.reducer';
 import candidatesReducer from './candidates/candidates.reducer';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['user']
-}
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['user']
+// }
 
-const rootReducer = combineReducers({
+export default combineReducers({
   user: userReducer,
   candidates: candidatesReducer
 });
 
-export default persistReducer(persistConfig, rootReducer);
+
+// const rootReducer = combineReducers({
+//   user: userReducer,
+//   candidates: candidatesReducer
+// });
+
+// export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+// import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';  //used for logging action details when ever any action gets fired
 
 import rootReducer from './root-reducer';
@@ -8,6 +8,7 @@ const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
-export default {store, persistor};
+// export default {store, persistor};
+export default store;
